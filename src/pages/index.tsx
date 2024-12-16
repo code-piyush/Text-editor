413b6b441dbe77ad8be9e0cd8c57e8e90a80371c
Piyush K <piyush.k.pixeldar@gmail.com>
2024-12-16 15:41:59 +0530

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,10 @@ import FullFeaturedEditor from '@/components/JoditEditor';
 const Index = () => {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Jodit Editor Full-Featured Demo</h1>
+      <h1 className="text-xl font-bold italic px-5">
+        Jodit Editor Full-Featured Demo
+      </h1>
+
       <FullFeaturedEditor />
     </div>
   );
